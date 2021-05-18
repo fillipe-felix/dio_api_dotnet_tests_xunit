d6d9eaf03710201b3593d4700054ed9e7e5bf6a7
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-17 22:34:52 -0300

    first commit

---
+++ dio_api_dotnet_tests_xunit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace dio_api_dotnet_tests_xunit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
